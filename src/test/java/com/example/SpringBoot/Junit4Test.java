6f68a0b8f0630c39b6fae8be677c6b1a15434475
lihonghui <lihonghui@xlpayment.com>
2019-09-28 10:37:02 +0800

    错误日志分开打印

--- src/test/java/com/example/SpringBoot/Junit4Test.java
+++ src/test/java/com/example/SpringBoot/Junit4Test.java
@@ -3,6 +3,8 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,6 +23,7 @@ import springBoot.Application;
 @SpringBootTest(classes = Application.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Junit4Test {
+	private static final Logger LOG = LoggerFactory.getLogger(Junit4Test.class);
 
 	@Autowired
 //	private TransContractServiceImpl transContractServiceImpl;
@@ -28,5 +31,15 @@ public class Junit4Test {
 	@Test
 	public void test01TransContract() {
 //		transContractServiceImpl.transContract();
+	}
+	/**
+	 * trace < debug < info < warn < error
+	 */
+	@Test
+	public void testLog(){
+		
+		LOG.info("---------info级别日志-------------");
+		LOG.warn("---------warn级别日志--------------");
+		LOG.error("----------error级别日志-----------");
 	}
 }
