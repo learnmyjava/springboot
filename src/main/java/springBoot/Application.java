67435979da9d483a41018c2841922904c707eba6
lihonghui <1499004754@qq.com>
2020-03-30 21:19:10 +0800

    自动注解原理

--- src/main/java/springBoot/Application.java
+++ src/main/java/springBoot/Application.java
@@ -9,8 +9,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * https://blog.csdn.net/u014745069/article/details/83820511
  * springboot 自动配置原理
  * @SpringBootApplication 是一个复合注解，其中之一的注解EnableAutoConfiguration，这个注解的作用就是开启自动配置，这个注解也是复合注解，
- * 其中关键注解@Import，其导入的AutoConfigurationImportSelector 实现了接口ImportSelector 的selectImports()方法,方法中去读取
- * META-INF/spring.factories 这个文件，此文件以类全限定名的方式 配置所有的springboot需要自动装载的类，容器根据全限定名来创建类并
+ * 其中关键注解@Import，其导入的AutoConfigurationImportSelector 实现了接口ImportSelector 的selectImports()方法,方法中从classpath中
+ * 搜索所有META-INF/spring.factories 文件并读取，此文件以类全限定名的方式 配置所有的springboot需要自动装载的类，根据全限定名来创建类并注册到容器
  * 
  * 与配置文件匹配的原理
  * 以AutoConfiguration结尾来命名的，实际上是个javaConfig 形势的spring容器配置类，此配置类通过@EnableConfigurationProperties 注解将XXXPropertiesde类加载到容器，
