2b31bc95fb61fdf31fe9830d7289623706bae2f6
lihonghui <1499004754@qq.com>
2020-06-23 18:07:46 +0800

    test

--- src/main/java/springBoot/controller/LoginController.java
+++ src/main/java/springBoot/controller/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import springBoot.Adapter.JwtInterceptor;
@@ -27,7 +28,7 @@ import springBoot.service.IuserInfoService;
 public class LoginController {
 	@Autowired
 	IuserInfoService userImpl;
-	
+
 	@Autowired
 	Constant c;
 	@RequestMapping("getstr")
@@ -35,20 +36,20 @@ public class LoginController {
 	public String getstr(){
 		return "hellojava";
 	}
-	@RequestMapping("/login") 
+	@RequestMapping("/login")
 	//跳转到指定页面时 @Controller 注解和视图解析器InternalResourceViewResolver 配合使用
 	//使用@RestController注解，视图解析器InternalResourceViewResolver会失效，无法解析html、jsp页面进而跳转到指定的页面，而是返回return的内容
 	//需要借助ModelAndView 返回页面
-	public String login(){
-		
+/*	public String login(){
+
 		return "login";
-	}
-/*	public ModelAndView Login(){
+	}*/
+	public ModelAndView Login(){
 		ModelAndView model = new ModelAndView();
 		model.setViewName("login/login");
 		return model;
-	}*/
-	
+	}
+
 	@RequestMapping("/loginVerify")//@Controller返回页面
 	public String loginVerify(User user,Model model,HttpSession session){
 		if(user == null || user.getUsername()==null || user.getPassword() == null){
@@ -57,45 +58,45 @@ public class LoginController {
 			return "login/login";
 		}
 		User u =userImpl.getUser(user);
-		
+
 		if( null != u){
 			log.info("当前登录用  username="+u.getUsername());
-			
+
 			//设置token
 			String token = JWTUtil.getToken(u.getId(), u.getUsername());
 			log.info("创建JWT："+token);
 			u.setToken(token);
 			session.setAttribute(JwtInterceptor.SESSION_USER_KEY, u);
 			model.addAttribute("user", u);
-			
-			
+
+
 			log.info("#############"+c.getC1()+""+c.getC2());//测试常量
 //			int i =9/0;//测试自定义异常
-			
-		
+
+
 			return "login/jspsuccess";
 		}else{
 			model.addAttribute("logintip", "用户名或密码错误");
 			model.addAttribute("user", user);
 			return "login/login";
 		}
-		
-		
+
+
 	}
 	@RequestMapping("/logoff")
 	public String logoff(HttpSession session){
 		session.removeAttribute(JwtInterceptor.SESSION_USER_KEY);
 		return "login/login";
 	}
-	
+
 	@RequestMapping("/ajaxrequest")
 	@ResponseBody
 	public User ajaxrequest(@RequestBody User u,HttpSession session){
 		session.setAttribute(JwtInterceptor.SESSION_USER_KEY, u);
 		return u;
 	}
-	
-	
+
+
 	@RequestMapping("/showList")
 	public String showList(Model model,HttpSession session){
 		List list=new ArrayList<>();
@@ -105,16 +106,16 @@ public class LoginController {
 			u1.setUsername("marray");
 			u1.setAddress("山东");
 			u1.setAge(12);
-			
-			
+
+
 			list.add(u1);
-			
+
 			list.add(u);
 		}
-	
-		
+
+
 		model.addAttribute("list", list);
 		return "login/showList";
-		
+
 	}
 }
